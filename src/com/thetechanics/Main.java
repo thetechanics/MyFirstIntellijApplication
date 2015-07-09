e6ddb8fc4b2f005707c1726b7dab29411efe5701
Head Techanic <headtechanic@gmail.com>
2015-07-09 12:53:52 -0500

    Third commit to GitHub

--- src/com/thetechanics/Main.java
+++ src/com/thetechanics/Main.java
@@ -3,7 +3,7 @@ package com.thetechanics;
 public class Main {
 
     public static void main(String[] args) {
-        //Comment to create fresh change to commit.
+        //Comment to create fresh change to commit - 20150709 12:52
         System.out.println (args[0]);
     }
 }
