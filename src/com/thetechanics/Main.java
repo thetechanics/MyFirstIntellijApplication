8845b4e8c0f353bcfc85faee9547985dfb2645a6
Head Techanic <headtechanic@gmail.com>
2015-07-09 12:42:12 -0500

    Second commit to GitHub projectd

--- src/com/thetechanics/Main.java
+++ src/com/thetechanics/Main.java
@@ -3,6 +3,7 @@ package com.thetechanics;
 public class Main {
 
     public static void main(String[] args) {
+        //Comment to create fresh change to commit.
         System.out.println (args[0]);
     }
 }
